df63b0e66a72953b21302881592cfdc5917cd942
Lech Gudalewicz <lech@aiki.io>
2016-10-25 21:35:55 -0700

    .

--- src/main/java/com/tibco/App.java
+++ src/main/java/com/tibco/App.java
@@ -1,33 +1,45 @@
 package com.tibco;
 
+import java.io.IOException;
 import java.util.Arrays;
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.cache.CacheMode;
+import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.configuration.IgniteConfiguration;
 import org.apache.ignite.spi.discovery.tcp.TcpDiscoverySpi;
 import org.apache.ignite.spi.discovery.tcp.ipfinder.vm.TcpDiscoveryVmIpFinder;
 
 public class App {
 
-  public static void main(String[] args) {
-    TcpDiscoverySpi spi = new TcpDiscoverySpi();
-    TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder();
-    ipFinder.setAddresses(Arrays.asList(args));
-    spi.setIpFinder(ipFinder);
+  private static final String CACHE_NAME = "myCache";
 
-    IgniteConfiguration configuration = new IgniteConfiguration();
-    configuration.setDiscoverySpi(spi);
+  public static void main(String[] args) throws IOException {
+    {
+      TcpDiscoverySpi spi = new TcpDiscoverySpi();
+      spi.setLocalPort(48500);
+      TcpDiscoveryVmIpFinder ipFinder = new TcpDiscoveryVmIpFinder();
 
-    Ignite ignite = Ignition.start(configuration);
-    IgniteCache<Integer, String> cache = ignite.getOrCreateCache("myCache");
-    for (int i = 0; i < 10; ++i) {
-      cache.put(i, Integer.toString(i));
+      ipFinder.setAddresses(Arrays.asList(args));
+
+      spi.setIpFinder(ipFinder);
+
+
+      IgniteConfiguration clusterConfig = new IgniteConfiguration();
+      CacheConfiguration cacheConfig = new CacheConfiguration();
+      clusterConfig.setDiscoverySpi(spi);
+      cacheConfig.setCacheMode(CacheMode.REPLICATED);
+      clusterConfig.setCacheConfiguration(cacheConfig);
+
+      Ignite ignite = Ignition.start(clusterConfig);
+      IgniteCache<String, String> cache = ignite.getOrCreateCache(CACHE_NAME);
+
+      cache.put("Hello", "World");
+
+      IgniteCache<String, String> c = ignite.getOrCreateCache(CACHE_NAME);
+      System.out.println(c.get("Hello"));
+      System.in.read();
     }
-
-    for (int i = 0; i < 10; ++i) {
-      System.out.println(cache.get(i));
-    }
-
   }
 }
